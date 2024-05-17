6b968c7f146a412db8213c922b6b89ba9cbc7d0a
nunu111 <nunu111za@hotmail.com>
2024-05-17 14:53:26 +0700

    .

--- stamp test folder/test.py
+++ stamp test folder/test.py
@@ -1,12 +1,19 @@
 import os
 import sys
 import tkinter as tk
+from bs4 import BeautifulSoup
 from tkinter import filedialog
 from tkinter import messagebox
 import urllib.request
-
-
+from urllib.parse import urlparse
+from datetime import datetime
+import whois
+import csv
+import requests
+import time
+import socket
 def run():
+    if(not checkFormatURL()): return
     title.place_forget()
     by.place_forget()
     internet_text.place_forget()
@@ -21,16 +28,19 @@ def run():
     button2.place_forget()
     button3.place_forget()
     button4.place_forget()
-
+    
     result_title.place(x=150, y=10)
     result_Label.place(x=150, y=50)
     internet_text.place(x=150, y=85)
     Internet_Label.place(x=235, y=85)
     config_text.place(x=150, y=110)
-    result_frame.place(x=100, y=140, width=500, height=250)
+    result_frame.place(x=60, y=140, width=600, height=250)
+    progress_Time.place(x=450, y=110)
     button2.place(x=270, y=400)
-    button5.place(x=370, y=400)
-
+    config_text.config(text="URL : "+str(URL_Entry.get())+" | Scan limit :"+str(Scan_Limit_Entry.get()))
+    rateLimit=  int(Scan_Limit_Entry.get()) if not Scan_Limit_Entry.get() == 'No Limit' else 0
+    find_defacement(URL_Entry.get(),"",rateLimit)
+    
 
 def back():
     result_title.place_forget()
@@ -56,9 +66,25 @@ def back():
     button2.place(x=410,y=215)
     button3.place(x=410,y=250)
     button4.place(x=410,y=430)
+    result_text.config(state=tk.NORMAL)
+    result_text.delete('1.0', tk.END)
+    result_text.config(state=tk.DISABLED)
+    
+def is_valid_domain(domain):
+    try:
+        socket.gethostbyname(domain)
+        return True
+    except socket.error:
+        return False
 
-
-
+def checkFormatURL():
+    website_url_sub1 = URL_Entry.get()
+    if not (website_url_sub1.startswith("https://") or website_url_sub1.startswith("http://")):
+        website_url_sub1 =  "https://" +website_url_sub1
+    if(URL_Entry.get() == "" or not is_valid_domain(urlparse(website_url_sub1).netloc)): 
+        messagebox.showerror("URL not valid","URL not valid or domain not found.")
+        return False
+    return True
 def open_file():
         try:
             with open('keyword.txt', 'r', encoding='utf-8') as file:
@@ -183,7 +209,7 @@ result_title.place_forget()
 result_Label = tk.Label(root, text="Result", font=('Arial', 16))
 result_Label.place_forget()
 
-config_text = tk.Label(root,text="URL : "+str(URL_Entry)+" | Scan limit :"+str(Scan_Limit_Entry))
+config_text = tk.Label(root,text="URL : "+str(URL_Entry.get())+" | Scan limit :"+str(Scan_Limit_Entry.get()))
 
 
 result_frame = tk.Frame(root)
@@ -192,8 +218,153 @@ result_frame.place_forget()
 result_text = tk.Text(result_frame, wrap='word')
 result_text.pack(fill=tk.BOTH, expand=True)
 
+progress_Time = tk.Label(root, text="Estimate Time: ")
+progress_Time.place_forget()
 
 root.after(0, update_status)
 root.after(0, open_file)
+
+def write_result(Domain,url_found,founding,url_notfound,url_cannot_fetch):
+    os.makedirs("./History", exist_ok=True)
+    current_date = datetime.now().date()
+    f = open(f"./History/{str(urlparse(Domain).netloc)}-{current_date}.txt", "w", encoding='utf-8')
+    f.write(f"{str(datetime.now().time())}\n")
+    f.write(str(get_domain_info(Domain)))
+    f.write("\n[Defacement detected]\n")
+    if(not url_found): f.write("-\n")
+    for i in range(len(url_found)):
+        f.write(f"{url_found[i]}\n")
+        f.write(f"found: {founding[i]}\n")
+
+    f.write("[No defacement detected]\n")
+    if(not url_notfound): f.write("-\n")
+    for i in url_notfound:
+        f.write(f"{i}\n")
+    f.write("[Cannot fetch URL]\n")
+    if(not url_cannot_fetch): f.write("-\n")
+    for i in url_cannot_fetch:
+        f.write(f"{i}\n")
+    f.write("------------------------------------------------------------------")
+    print("Finish")
+
+def get_domain_info(domain_name):
+    try:
+        domain_info = whois.whois(domain_name)
+        return domain_info
+    except Exception as e:
+        return f"An error occurred: {e}"
+    
+def fetch_website_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else: #! cannot access to website
+            result_text.insert(tk.END,f"Failed to fetch website content. Status code: {response.status_code}, on the website {url}")
+            result_text.see(tk.END)
+
+            return None
+    except Exception as e:
+        result_text.insert(tk.END,f"Error fetching website content: on the website {url}\n", e)
+        result_text.see(tk.END)
+        return None
+
+def find_defacement(url,url_main_sub,rateLimit=3):
+    #* Check HTML content variable
+    found =[]
+    paths = ['']
+    url_found =[]
+    founding=[]
+    url_notfound =[]
+    url_cannot_fetch = []
+
+    #* Passive scan variable
+    fetched =set()
+    fetch_domain = url.strip()
+    sub_fetch_domain = url_main_sub.strip()
+    isNotFinish = True
+    paths = [fetch_domain+sub_fetch_domain]
+    limit =1
+    estimate_time=0
+    
+    root.update()
+    
+    try:
+        open('keyword.txt', "x")
+        print('keyword.txt does not exits.\nNow it was created please write keyword in file')
+        return
+    except FileExistsError:
+        while(isNotFinish and (limit <= rateLimit or rateLimit==0)):
+            
+            result_text.update()
+            
+            progress_Time.config(text=f"Estimate time: {round(estimate_time, 2)} sec")
+            start_time = time.time()
+            
+            if len(paths) ==  0: break
+            else : fetch_url = paths.pop(0)
+            found =[]
+            website_content = fetch_website_content(fetch_url)
+            fetched.add(fetch_url)
+
+            if(website_content == None ): 
+                url_cannot_fetch.append(fetch_url)
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                estimate_time -= elapsed_time
+                continue
+            soup = BeautifulSoup(website_content, 'html.parser')
+            links = soup.find_all('a', href=True)
+            with open('keyword.txt','r', encoding='utf-8') as file:
+                csv_reader = csv.reader(file)
+                for keyword in csv_reader:
+                    if len(keyword)==0 : continue
+                    if soup.find(string=lambda text: text and keyword[0] in text):
+                        found.append(keyword[0])
+                if(found) :
+                    result_text.config(state=tk.NORMAL)
+                    result_text.insert(tk.END,f"Defacement detected on the website: {fetch_url}\n")
+                    result_text.insert(tk.END,f'summary keyword that were found : {found}\n')
+                    url_found.append(fetch_url)
+                    founding.append(found)
+                    result_text.see(tk.END)
+                    result_text.config(state=tk.DISABLED)
+
+                else : 
+                    result_text.config(state=tk.NORMAL)
+                    result_text.insert(tk.END,f"No defacement detected on the website: {fetch_url}\n")
+                    url_notfound.append(fetch_url)
+                    result_text.see(tk.END)
+                    result_text.config(state=tk.DISABLED)
+
+            for link in links:
+                path = link['href']
+                path =path.strip()
+                if(path == None) : continue
+                if (len(path) > 1):
+                    if(path.startswith('/')):
+                        path = url+path
+                    elif(path.startswith(url)):
+                        path = path
+                    else :continue
+                    if not (path.endswith('.pdf') or path.endswith('.jpg') or path.endswith('.png')):
+                        if  ((path not in paths)and( path not in fetched)):
+                            paths.append(path)
+            limit += 1
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            estimate_time = elapsed_time*len(paths) if len(paths)<limit or rateLimit==0 else elapsed_time*(rateLimit-limit+1)
+        
+        # self.progress_Time.config(text="Finish")
+        result_text.config(state=tk.NORMAL)
+        result_text.insert(tk.END,f"Finish")
+        result_text.see(tk.END)
+        result_text.config(state=tk.DISABLED)
+        progress_Time.config(text="Finish")
+        write_result(url,url_found,founding,url_notfound,url_cannot_fetch)
+    button5.place(x=370, y=400)
+
+    # self.back_button.pack(pady=20)
+
 
 root.mainloop()
